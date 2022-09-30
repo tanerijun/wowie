acf6dc6e064ce37531dddf353dac268447c34d35
tanerijun <tanerivince@gmail.com>
2022-09-30 14:19:36 +0800

    Fix Header's styling on mobile

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -31,7 +31,7 @@ export default function SearchInput({
         onChange={handleInputChange}
         value={searchQuery}
         placeholder="Search ..."
-        className="text-md h-10 border-b border-slate-400 bg-transparent p-4 pr-14 text-gray-50 caret-gray-50 placeholder:text-slate-400 focus:border-cyan-200 focus:outline-none md:w-96"
+        className="text-md h-10 w-5/6 border-b border-slate-400 bg-transparent p-4 pr-14 text-gray-50 caret-gray-50 placeholder:text-slate-400 focus:border-cyan-200 focus:outline-none sm:w-96"
       />
       <div className="absolute right-4 h-8 w-8">
         <Image
