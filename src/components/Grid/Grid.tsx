44cbbf827006fd603274346413993580fdf68ae8
tanerijun <tanerivince@gmail.com>
2023-07-28 15:47:35 +0800

    Styling improvement

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -7,7 +7,7 @@ export default function Grid({ title, children }: Props) {
   return (
     <section className="m-auto mt-8 max-w-7xl p-4">
       <h2 className="pb-4 text-xl font-bold text-gray-100">{title}</h2>
-      <div className="grid grid-cols-auto-fill gap-8">{children}</div>
+      <div className="grid grid-cols-auto-fill gap-2">{children}</div>
     </section>
   );
 }
